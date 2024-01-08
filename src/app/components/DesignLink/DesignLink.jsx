65fb2e4c678c72814901fbe18761edfad43225c2
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-08 16:57:33 +0200

    add design pages

--- src/app/components/DesignLink/DesignLink.jsx
+++ src/app/components/DesignLink/DesignLink.jsx
@@ -1,13 +1,14 @@
+import Link from 'next/link'
 import './DesignLink.css'
 
 export default function DesignLink( {design, large = false} ) {
     return (
-        <div className={`design-link text-center ${large ? "webLink" : ""} ${design.bgImage}`}>
+        <Link href={design.url} className={`design-link text-center ${large ? "webLink" : ""} ${design.bgImage}`}>
             <h2 className='z-10'>{design.title}</h2>
             <div className='flex gap-[1.25rem] items-center justify-center z-10'>
                 <p className="uppercase tracking-[.25em] text-[15px]">View project</p>
                 <img src='/icon-right-arrow.svg' alt='arrow image'/>
             </div>
-        </div>
+        </Link>
     )
 }
