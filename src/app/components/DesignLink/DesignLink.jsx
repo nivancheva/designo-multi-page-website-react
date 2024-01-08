de1ec748adfd8b1ba91adbcf49f28b08e60104e2
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-08 16:14:19 +0200

    fix img src for arrow image

--- src/app/components/DesignLink/DesignLink.jsx
+++ src/app/components/DesignLink/DesignLink.jsx
@@ -1,6 +1,4 @@
 import './DesignLink.css'
-import arrowImg from '../../assets/icon-right-arrow.svg'
-import Image from 'next/image'
 
 export default function DesignLink( {design, large = false} ) {
     return (
@@ -8,7 +6,7 @@ export default function DesignLink( {design, large = false} ) {
             <h2 className='z-10'>{design.title}</h2>
             <div className='flex gap-[1.25rem] items-center justify-center z-10'>
                 <p className="uppercase tracking-[.25em] text-[15px]">View project</p>
-                <Image src={arrowImg} alt='arrow image'/>
+                <img src='/icon-right-arrow.svg' alt='arrow image'/>
             </div>
         </div>
     )
