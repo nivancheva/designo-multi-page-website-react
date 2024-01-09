090bfeafe339c164cf320a62cffa6e61893910d5
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-09 11:25:11 +0200

    add graphic design page content

--- src/app/design/graphic/page.jsx
+++ src/app/design/graphic/page.jsx
@@ -1,7 +1,26 @@
+import DesignLink from '../../components/DesignLink/DesignLink'
+import designsJson from '../../../designs.json'
+import GetInTouch from '../../components/GetInTouch/GetInTouch'
+
 export default function GhraphicDesign() {
     return (
-        <div>
-            Graphic Design
+        <div className=''>
+            <div className='design-heading'>
+                <h1>Graphic Design</h1>
+                <p>We deliver eye-catching branding materials that are tailored to meet your business objectives.</p>
+            </div>
+            <div className='section-gap container'>
+                Cards
+            </div>
+
+            <div className='links-designs grid section-gap container'>
+                <DesignLink design={designsJson.appDesign}/>
+                <DesignLink design={designsJson.webDesign}/>
+            </div>
+
+            <div className='translateY container'>
+                <GetInTouch />
+            </div>
         </div>
     )
 }
