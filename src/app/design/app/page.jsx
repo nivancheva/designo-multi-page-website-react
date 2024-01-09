21426fe6d6dfe6a8effc09f683f8f8c29c3120ba
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-09 11:22:58 +0200

    add app design page content

--- src/app/design/app/page.jsx
+++ src/app/design/app/page.jsx
@@ -1,7 +1,26 @@
+import DesignLink from '../../components/DesignLink/DesignLink'
+import designsJson from '../../../designs.json'
+import GetInTouch from '../../components/GetInTouch/GetInTouch'
+
 export default function AppDesign() {
     return (
-        <div>
-            App Design
+        <div className=''>
+            <div className='design-heading'>
+                <h1>App Design</h1>
+                <p>Our mobile designs bring intuitive digital solutions to your customers right at their fingertips.</p>
+            </div>
+            <div className='section-gap container'>
+                Cards
+            </div>
+
+            <div className='links-designs grid section-gap container'>
+                <DesignLink design={designsJson.webDesign}/>
+                <DesignLink design={designsJson.graphicDesign}/>
+            </div>
+
+            <div className='translateY container'>
+                <GetInTouch />
+            </div>
         </div>
     )
 }
