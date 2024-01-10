9fea5a9c957d002341ce6b3d8946988096a8b948
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-10 11:15:54 +0200

    add pictures to Compony component

--- src/app/company/page.jsx
+++ src/app/company/page.jsx
@@ -6,7 +6,18 @@ export default function Company() {
     return (
         <div className="container">
             <div>
-                <img />
+                <picture>
+                    <source
+                        srcSet="/about/desktop/image-about-hero.jpg"
+                        media="(min-width:864px)"
+                    />
+                    <source
+                        srcSet="/about/tablet/image-about-hero.jpg"
+                        media="(min-width:640px)"
+                    />
+                    <img src="/about/mobile/image-about-hero.jpg" />
+                </picture>
+              
                 <div>
                     <h2>About Us</h2>
                     <p>Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients' audiences.</p>
@@ -14,7 +25,18 @@ export default function Company() {
             </div>
 
             <div className='section-gap'>
-                <img />
+                <picture>
+                    <source
+                        srcSet="/about/desktop/image-world-class-talent.jpg"
+                        media="(min-width:864px)"
+                    />
+                    <source
+                        srcSet="/about/tablet/image-world-class-talent.jpg"
+                        media="(min-width:640px)"
+                    />
+                    <img src="/about/mobile/image-world-class-talent.jpg" />
+                </picture>
+
                 <div>
                     <h2>World-class talent</h2>
                     <p>We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.</p>
@@ -25,7 +47,18 @@ export default function Company() {
             <Countries />
 
             <div className='section-gap'>
-                <img />
+                <picture>
+                    <source
+                        srcSet="/about/desktop/image-real-deal.jpg"
+                        media="(min-width:864px)"
+                    />
+                    <source
+                        srcSet="/about/tablet/image-real-deal.jpg"
+                        media="(min-width:640px)"
+                    />
+                    <img src="/about/mobile/image-real-deal.jpg" />
+                </picture>
+
                 <div>
                     <h2>The real deal</h2>
                     <p>As strategic partners in our clients' businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
