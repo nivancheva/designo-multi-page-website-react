b8c315064fcafb79451c5bdca981f410a799f585
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-12 13:33:08 +0200

    add form validation

--- src/app/components/Form/Form.jsx
+++ src/app/components/Form/Form.jsx
@@ -1,36 +1,57 @@
-import './Form.css'
+"use client"
 
+import './Form.css'
+import { z } from 'zod'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+    name: z.string().min(1, "Can't be empty"),
+    email: z.string().min(1, "Can't be empty").email({ message: "Please enter a valid email address." }),
+    phone: z.coerce.number(),
+    message: z.string().min(1, "Can't be empty")
+})
+ 
 export default function Form() {
+    
+
+    function handleFormSubmit(data) {
+        console.log(data);
+    }
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+      } = useForm({ resolver: zodResolver(schema) });
+
     return (
-        <form className='grid gap-[1.5rem]'>
+        <form className='grid gap-[1.5rem]' onSubmit={handleSubmit(handleFormSubmit)}>
             <input 
                 className='block'
-                type="text"
-                id="name"
-                name="name"
                 placeholder='Name'
+                {...register("name")}
             />
+            {errors.name && <p>{errors.name.message}</p>}
             <input
                 className='block'
-                type="text"
-                id="email"
-                name="email"
                 placeholder='Email Adress'
+                {...register("email")}
             />
+            {errors.email && <p>{errors.email.message}</p>}
             <input
                 className='block'
-                type="text"
-                id="phone"
-                name="phone"
+                type="tel"
                 placeholder='Phone'
+                {...register("phone")}
             />
+            {errors.phone && <p>{errors.phone.message}</p>}
             <textarea
                 className='block message-input'
-                type="text"
-                id="message"
-                name="message"
                 placeholder='Your message'
+                {...register("message")}
             />
+            {errors.message && <p>{errors.message.message}</p>}
             <button className='button-secondary form-button'>Submit</button>
         </form>
     )
