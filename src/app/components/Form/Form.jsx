d067ecfee5753d190a873288bfdad8dadf94e42b
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-12 14:03:37 +0200

    style error message

--- src/app/components/Form/Form.jsx
+++ src/app/components/Form/Form.jsx
@@ -4,6 +4,7 @@ import './Form.css'
 import { z } from 'zod'
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import iconError from '../../../../public/contact/desktop/icon-error.svg'
 
 const schema = z.object({
     name: z.string().min(1, "Can't be empty"),
@@ -27,31 +28,64 @@ export default function Form() {
 
     return (
         <form className='grid gap-[1.5rem]' onSubmit={handleSubmit(handleFormSubmit)}>
-            <input 
-                className='block'
-                placeholder='Name'
-                {...register("name")}
-            />
-            {errors.name && <p>{errors.name.message}</p>}
-            <input
-                className='block'
-                placeholder='Email Adress'
-                {...register("email")}
-            />
-            {errors.email && <p>{errors.email.message}</p>}
-            <input
-                className='block'
-                type="tel"
-                placeholder='Phone'
-                {...register("phone")}
-            />
-            {errors.phone && <p>{errors.phone.message}</p>}
-            <textarea
-                className='block message-input'
-                placeholder='Your message'
-                {...register("message")}
-            />
-            {errors.message && <p>{errors.message.message}</p>}
+
+            <div className='relative'>
+                <input 
+                    className='block'
+                    placeholder='Name'
+                    {...register("name")}
+                />
+                {errors.name && 
+                    <div className='errors flex'>
+                        <p>{errors.name.message}</p>
+                        <div><img src='/contact/desktop/icon-error.svg'/></div>
+                    </div>
+                }
+            </div>
+
+            <div className='relative'>
+                <input
+                    className='block'
+                    placeholder='Email Adress'
+                    {...register("email")}
+                />
+                {errors.email && 
+                    <div className='errors flex'>
+                        <p>{errors.email.message}</p>
+                        <div><img src='/contact/desktop/icon-error.svg'/></div>
+                    </div>
+                }
+            </div>
+
+            <div className='relative'>
+                <input
+                    className='block'
+                    type="tel"
+                    placeholder='Phone'
+                    {...register("phone")}
+                />
+                {errors.phone && 
+                    <div className='errors flex'>
+                        <p>{errors.phone.message}</p>
+                        <div><img src='/contact/desktop/icon-error.svg'/></div>
+                    </div>
+                }
+            </div>
+            
+            <div className='relative'>
+                <textarea
+                    className='block message-input'
+                    placeholder='Your message'
+                    {...register("message")}
+                />
+                {errors.message && 
+                    <div className='errors flex'>
+                        <p>{errors.message.message}</p>
+                        <div><img src='/contact/desktop/icon-error.svg'/></div>
+                    </div>
+                }
+            </div>
+            
             <button className='button-secondary form-button'>Submit</button>
         </form>
     )
