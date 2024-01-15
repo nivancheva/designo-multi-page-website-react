53422a363701f7c176af0a6749d165db9337e92b
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-01-15 10:37:10 +0200

    fix apostrophes

--- src/app/components/GetInTouch/GetInTouch.jsx
+++ src/app/components/GetInTouch/GetInTouch.jsx
@@ -4,7 +4,7 @@ export default function GetInTouch() {
     return (
         <div className='get-in-touch'>
             <div className='get-in-touch_heading grid gap-4'>
-                <h2>Let’s talk about your project</h2>
+                <h2>Let's talk about your project</h2>
                 <p className='opacity-90'>Ready to take it to the next level? Contact us today and find out 
                     how our expertise can help your business grow.
                 </p>
